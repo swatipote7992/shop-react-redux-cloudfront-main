161671eb9ece26fcf0403d70ec16f59efd2fa8ef
swatipote7992 <swatitpacc1@gmail.com>
2025-04-30 11:34:42 +0100

    passing product by azfunct

--- fa-products-service/http-get-product-by-id/index.ts
+++ fa-products-service/http-get-product-by-id/index.ts
@@ -1,15 +1,14 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { products } from "../mock/products";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    const productId = (req.params && req.params.productId);
+    const selectedProduct = products.filter(p => p.id.toString() === productId);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 200, // HTTP Status Code
+        headers: { "Content-Type": "application/json" },
+        body: selectedProduct // JSON response
     };
 
 };
--- fa-products-service/http-get-product-list/index.ts
+++ fa-products-service/http-get-product-list/index.ts
@@ -1,15 +1,13 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { products } from "../mock/products";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    const selectedProducts = products;
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 200, // HTTP Status Code
+        headers: { "Content-Type": "application/json" },
+        body: selectedProducts // JSON response
     };
 
 };

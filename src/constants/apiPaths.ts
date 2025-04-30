bab37b10ccd6026a39c71d2fb228a53270fc4ee1
swatipote7992 <swatitpacc1@gmail.com>
2025-04-30 12:17:17 +0100

    calling api from fe app

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://productserviceapitest.azure-api.net/fa-azureproducts-service-ne-002",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
